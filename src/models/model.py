82b92115469c5c862ccd2bec1e5b691c40cc7789
Faroukelmak <135977755+Faroukelmak@users.noreply.github.com>
2025-02-10 19:29:32 +0100

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 @st.cache_resource
-def load_yolo_model(yolo_path="../../models/best.pt"):
+def load_yolo_model(yolo_path="best.pt"):
     """Load the YOLO model for face detection."""
     from ultralytics import YOLO
     return YOLO(yolo_path)
@@ -18,7 +18,7 @@ def load_yolo_model(yolo_path="../../models/best.pt"):
 from tensorflow.keras.losses import MeanSquaredError
 
 def load_cnn_model():
-    cnn_path = '../../models/tinyvgg_utkface.h5'  # Update with your actual model path
+    cnn_path = 'tinyvgg_utkface.h5'  # Update with your actual model path
     # Loading the model and passing custom_objects
     
     cnn_model = tf.keras.models.load_model(cnn_path, custom_objects={'mse': MeanSquaredError()}, compile=False)
